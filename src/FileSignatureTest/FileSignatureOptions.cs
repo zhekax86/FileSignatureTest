4646f00495c9a41c2b72d2d41459f367afaacce0
Evgeniy Tipikin <_x86@rambler.ru>
2022-08-25 16:44:57 +0700

    init project

---
+++ src/FileSignatureTest/FileSignatureOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSignatureTest
+{
+    internal class FileSignatureOptions
+    {
+        [Value(0, MetaName = "input file", HelpText = "File to calculate signature", Required = true)]
+        public string InputFile { get; set; }
+
+        [Option('b', "blockSize", HelpText = "Signature block size", Required = true )]
+        public int BlockSize { get; set; }
+    }
+}
---
+++ src/FileSignatureTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using FileSignatureTest;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var parserResults = Parser.Default.ParseArguments<FileSignatureOptions>(args)
+            .WithParsed(Run);
+    }
+
+    private static void Run(FileSignatureOptions options)
+    {
+
+    }
+}
